08e418e0f8670109dd9d569ee4604d256b26bd2b
chaurasiyarnc-a11y <chaurasiyarnc@gmail.com>
2026-07-07 07:01:43 +0530

    Update Firebase configuration with hardcoded values
    Replaced Firebase configuration with hardcoded values.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,18 @@
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// ये वैल्यूज़ Vercel के Environment Variables से ऑटोमैटिक आएंगी
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: AIzaSyCaVEwWnh7_-uDWcvOlFmfymy7kiSxbyMI, 
+  authDomain: gen-lang-client-0229168883.firebaseapp.com,
+  projectId: gen-lang-client-0229168883,
+  storageBucket: gen-lang-client-0229168883.firebasestorage.app,
+  messagingSenderId: 1051643029309,
+  appId: 1:1051643029309:web:3f5a9f9b7f981eaf814754
 };
 
-// Firebase को इनिशियलाइज़ करना (Singleton Pattern)
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
