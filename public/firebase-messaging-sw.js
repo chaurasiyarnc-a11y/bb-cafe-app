bef1d556b30e6801205b5a2643a3cd8879f2ce78
chaurasiyarnc-a11y <chaurasiyarnc@gmail.com>
2026-07-15 18:49:15 +0530

    Add Firebase messaging service worker

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBROy-Nj2Aaq2nzaVZnTfovFkWHA3HpMOE", 
+  authDomain: "gen-lang-client-0229168883.firebaseapp.com",
+  projectId: "gen-lang-client-0229168883",
+  storageBucket: "gen-lang-client-0229168883.firebasestorage.app",
+  messagingSenderId: "1051643029309",
+  appId: "1:1051643029309:web:3f5a9f9b7f981eaf814754"
+});
+
+var messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[BG sw.js] Received message: ', payload);
+  
+  var title = payload.notification.title || 'नया आर्डर तैयार है! 🛵';
+  var options = {
+    body: payload.notification.body || 'कृपया आर्डर विवरण की जांच करें और वितरण के लिए निकलें।',
+    icon: '/icon-192x192.png',
+    badge: '/icon-192x192.png',
+    vibrate: [200, 100, 200, 100, 200, 100, 200],
+    data: {
+      url: payload.data?.url || '/delivery'
+    }
+  };
+
+  return self.registration.showNotification(title, options);
+});
