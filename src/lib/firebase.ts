121bc80284d29188558674f6173f08afb6edafe7
chaurasiyarnc-a11y <chaurasiyarnc@gmail.com>
2026-07-15 18:55:41 +0530

    Initialize Firebase app and services

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBROy-Nj2Aaq2nzaVZnTfovFkWHA3HpMOE", 
+  authDomain: "gen-lang-client-0229168883.firebaseapp.com",
+  projectId: "gen-lang-client-0229168883",
+  storageBucket: "gen-lang-client-0229168883.firebasestorage.app",
+  messagingSenderId: "1051643029309",
+  appId: "1:1051643029309:web:3f5a9f9b7f981eaf814754"
+};
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
