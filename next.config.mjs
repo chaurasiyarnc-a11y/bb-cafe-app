39f174cb87f783be7a2f623a6384a16ae3bf397d
chaurasiyarnc-a11y <chaurasiyarnc@gmail.com>
2026-07-28 02:04:39 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
-
 /** @type {import('next').NextConfig} */
+// यह चेक करेगा कि क्या हम मोबाइल APK बना रहे हैं
+const isStaticExport = process.env.STATIC_EXPORT === 'true';
+
 const nextConfig = {
   reactStrictMode: true,
+  
+  // यदि मोबाइल बिल्ड चल रहा है, तो ही एक्सपोर्ट सेटिंग्स लागू करें
+  ...(isStaticExport ? {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
+  } : {})
 };
 
 export default nextConfig;
