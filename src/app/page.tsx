a7126d38aadc0ca157dce8f47fc6a1512a200a68
chaurasiyarnc-a11y <chaurasiyarnc@gmail.com>
2026-07-29 11:51:51 +0530

    Change CartDrawer import path to home directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ import { useRouter } from 'next/navigation';
 // सब-कंपोनेंट्स इम्पोर्ट्स
 import CategorySlider from '../components/home/CategorySlider';
 import DiyPizzaBuilder from '../components/home/DiyPizzaBuilder';
-import CartDrawer from '../components/admin/CartDrawer';
+import CartDrawer from '../components/home/CartDrawer';
 import UpiPaymentModal from '../components/home/UpiPaymentModal';
 
 const FALLBACK_CATEGORIES = ["All", "Special Pizza", "Special Thali", "Paneer Special", "Special Mix veg", "Fast Food", "Super Cool", "Indian Bread", "Special Rice"];
